1ae369f4044e68939bd96d02c48ba7d40038f98c
developerjosephutrera <developer.josephutrera@gmail.com>
2026-09-24 14:19:27 -0400

    correciones a los enlaces del blog

--- data/sections/blog1.ts
+++ data/sections/blog1.ts
@@ -13,7 +13,7 @@ export const blog1Data = {
             readTime: "Proyecto personal",
             // description: "Siempre me ha gustado investigar componentes, comparar opciones y armar mis propios equipos. Mi PC principal combina un Intel Core i5-10600K, refrigeración líquida AZZA de 240 mm y una ASUS RTX 5060. Más que tener un equipo potente, disfruto todo el proceso de configurarlo, optimizarlo y seguir mejorándolo.",
             image: "/assets/imgs/blog/blog-1/setup.png",
-            link: "/blog-details"
+            link: "#" //tiene que mandar a blog-details
         },
         {
             id: 2,
@@ -23,7 +23,7 @@ export const blog1Data = {
             readTime: "Aprendizaje continuo",
             // description: "Me gusta mantenerme al día con nuevos procesadores, tarjetas gráficas, periféricos y herramientas digitales. Seguir la evolución del hardware no solo es un interés personal; también me permite comprender mejor aspectos como rendimiento, compatibilidad, optimización y la relación entre el software y el equipo donde se ejecuta.",
             image: "/assets/imgs/blog/blog-1/tech.png",
-            link: "/blog-details"
+            link: "#" //tiene que mandar a blog-details
         },
         {
             id: 3,
@@ -33,7 +33,7 @@ export const blog1Data = {
             readTime: "Hobby",
             // description: "Los videojuegos han sido uno de mis pasatiempos durante años, especialmente títulos competitivos como Call of Duty: Warzone. También tuve la oportunidad de experimentar con creación de contenido, realizando streams en Twitch y compartiendo clips en TikTok. Es un espacio que disfruto por diversión y por todo lo que implica a nivel creativo y técnico.",
             image: "/assets/imgs/blog/blog-1/gaming.png",
-            link: "/blog-details"
+            link: "#" //tiene que mandar a blog-details
         }
     ]
 };
